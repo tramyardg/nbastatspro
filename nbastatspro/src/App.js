f4b761a6c36513cff4791b6d381f57753e48a676
Raymart De Guzman <tramyardg@gmail.com>
2018-07-06 21:45:00 -0400

    added call to search div

--- nbastatspro/src/App.js
+++ nbastatspro/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import './css/blog.css';
 import AppHeader from "./AppHeader";
+import CallToSearch from "./CallToSearch";
 import AppFooter from "./AppFooter";
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
     return (
         <div className="container">
           <AppHeader />
+          <CallToSearch/>
           <AppFooter/>
         </div>
     );
--- nbastatspro/src/CallToSearch.js
+++ nbastatspro/src/CallToSearch.js
@@ -1,1 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+class CallToSearch extends Component {
+  render() {
+    return (<div>
+      <div className="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+        <div className="col-md-6 px-0">
+          <h1 className="display-4 font-italic">Search your favorite player and see his statistic.</h1>
+          <p className="lead my-3">This app simply determines NBA player statistics by typing his first name and last name in the form.</p>
+        </div>
+      </div>
+    </div>);
+  }
+}
+
+export default CallToSearch;
