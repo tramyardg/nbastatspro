7d87f85370b7ef09f4db8901ced8e185188043e2
Raymart De Guzman <tramyardg@gmail.com>
2018-07-09 23:04:33 -0400

    #1 player card's component

--- nbastatspro/src/App.js
+++ nbastatspro/src/App.js
@@ -2,9 +2,11 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './css/blog.css';
+import './css/player.css';
 import AppHeader from "./AppHeader";
 import CallToSearch from "./CallToSearch";
 import SearchForm from './SearchForm';
+import PlayerProfile from './PlayerProfile';
 import AppFooter from "./AppFooter";
 
 /*
@@ -28,6 +30,7 @@ class App extends Component {
           <AppHeader />
           <CallToSearch/>
           <SearchForm/>
+          <PlayerProfile/>
           <AppFooter/>
         </div>
     );
--- nbastatspro/src/PlayerProfile.js
+++ nbastatspro/src/PlayerProfile.js
@@ -1,12 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import playerPhoto from './img_template_method_uml.png';
+
 
 const PlayerProfile = (props) => {
   return (
-      <div></div>
+      <div className="player-profile-container">
+        <h2>LeBron Jame<span>'s</span> Profile</h2>
+        <div className="container player-profile box-rounded">
+          <div className="row align-items-start">
+            <div className="col-sm-4">
+              <img src={playerPhoto} className="player-photo" alt="player photo" />
+            </div>
+            <div className="col-sm">
+              <div className="player-info-block">
+                <span>Name</span>
+                <span>LeBron James</span>
+              </div>
+              <div className="player-info-block">
+                <span>Current Team</span>
+                <span>LeBron James</span>
+              </div>
+              <div className="player-info-block">
+                <span>Jersey Number</span>
+                <span>LeBron James</span>
+              </div>
+              <div className="player-info-block">
+                <span>Position(s)</span>
+                <span>Small Guard</span>
+              </div>
+            </div>
+            <div className="col-sm">
+              <div className="player-info-block">
+                <span>Age</span>
+                <span>32</span> years old
+              </div>
+              <div className="player-info-block">
+                <span>Height</span>
+                <span>192</span>cm
+              </div>
+              <div className="player-info-block">
+                <span>Weight</span>
+                <span>81</span>kg
+              </div>
+              <div className="player-info-block">
+                <span>Nationality</span>
+                <span>American</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
   );
 };
 
+export default PlayerProfile;
 // PlayerProfile.defaultProps = {
 //
 // };
