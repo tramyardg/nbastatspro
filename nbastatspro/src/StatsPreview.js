2fecfdc0defacabd047d8074bdb27e91a80703ac
Raymart De Guzman <tramyardg@gmail.com>
2018-07-11 23:00:09 -0400

    #2 important avg. stats of a player

---
+++ nbastatspro/src/StatsPreview.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+/**
+ * will display avg:
+ * ppg, rpg, asp, stl, min, FG_PCT, FG3_PCT, FT_PCT, tov, pts, rank
+ */
+class StatsPreview extends Component {
+  render() {
+    return (
+        <div></div>
+    )
+  }
+}
